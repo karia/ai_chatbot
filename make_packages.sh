abfb0caaf5a40e3891fdd2dafabd2811bff4762b
karia <karia@side2.net>
2024-07-10 00:30:25 +0900

    Delete unused package

--- make_packages.sh
+++ make_packages.sh
@@ -5,7 +5,7 @@ set -eu
 # install python packages
 # ref https://zenn.dev/galapagos/articles/a222e38a32f4ba
 
-pip install -t ./package --upgrade fastapi==0.99.0 anthropic slack_sdk
+pip install -t ./package --upgrade slack_sdk
 
 # make deployment package
 # original from https://docs.aws.amazon.com/ja_jp/lambda/latest/dg/python-package.html
